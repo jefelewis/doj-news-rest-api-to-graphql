76a37711a8a91e4997a8fb1d1859df9b481b2301
Jeff Lewis <jeffylew77@gmail.com>
2018-07-25 14:11:35 -0700

    [Dev] Adding types and resolvers for DOJ blog entry, press release, speech, and vacancy announcement data

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -6,49 +6,69 @@ import axios from 'axios';
 const RESOLVERS = {
   Query: {
 
-    // 
+    // DEPARTMENT OF JUSTICE (DOJ) BLOG ENTRIES
+    // Get Last Ten Blog Entries
     getLastTenBlogEntries: (parent, args) => {
       return axios.get(`http://www.justice.gov/api/v1/blog_entries.json?&pagesize=10`)
       .then((response) => response.data)
-      .catch((error) => console.log(error));
+      .catch((error) => console.log(error))
     },
+    // Get Blog Entries by ID
     getBlogEntryByID: (parent, args) => {
       return axios.get(`http://www.justice.gov/api/v1/blog_entries/${args.ID}.json?&pagesize=1`)
       .then((response) => response.data)
-      .catch((error) => console.log(error));
+      .catch((error) => console.log(error))
     },
+
+
+
+    // DEPARTMENT OF JUSTICE (DOJ) PRESS RELEASES
+    // Get Last Ten Press Releases
     getLastTenPressReleases: (parent, args) => {
       return axios.get(`http://www.justice.gov/api/v1/press_releases.json?&pagesize=10`)
       .then((response) => response.data)
-      .catch((error) => console.log(error));
+      .catch((error) => console.log(error))
     },
+    // Get Press Release by ID
     getPressReleaseByID: (parent, args) => {
       return axios.get(`http://www.justice.gov/api/v1/press_releases/${args.ID}.json?&pagesize=1`)
       .then((response) => response.data)
-      .catch((error) => console.log(error));
+      .catch((error) => console.log(error))
     },
-    getLastTenBlogSpeeches: (parent, args) => {
+
+
+
+    // DEPARTMENT OF JUSTICE (DOJ) PRESS RELEASES
+    // Get Last Ten Speeches
+    getLastTenSpeeches: (parent, args) => {
       return axios.get(`http://www.justice.gov/api/v1/speeches.json?&pagesize=10`)
       .then((response) => response.data)
-      .catch((error) => console.log(error));
+      .catch((error) => console.log(error))
     },
+    // Get Speech by ID
     getSpeechByID: (parent, args) => {
       return axios.get(`http://www.justice.gov/api/v1/speeches/${args.ID}.json?&pagesize=1`)
       .then((response) => response.data)
-      .catch((error) => console.log(error));
+      .catch((error) => console.log(error))
     },
+
+
+
+    // DEPARTMENT OF JUSTICE (DOJ) VACANCY ANNOUNCEMENTS
+    // Get Last Ten Vacancy Announcments
     getLastTenVacancyAnnouncements: (parent, args) => {
       return axios.get(`http://www.justice.gov/api/v1/vacancy_announcements.json?&pagesize=10`)
       .then((response) => response.data)
-      .catch((error) => console.log(error));
+      .catch((error) => console.log(error))
     },
-    getVacancyAnnouncementsByID: (parent, args) => {
+    // Get Vacancy Announcment by ID
+    getVacancyAnnouncementByID: (parent, args) => {
       return axios.get(`http://www.justice.gov/api/v1/vacancy_announcements/${args.ID}.json?&pagesize=1`)
       .then((response) => response.data)
-      .catch((error) => console.log(error));
+      .catch((error) => console.log(error))
     }
   }
-};
+}
 
 // Exports
-export default RESOLVERS;
+export default RESOLVERS
