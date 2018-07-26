98a33e78eb2b0ace3307576db7cdd5229b6a4dea
Jeff Lewis <jeffylew77@gmail.com>
2018-07-26 12:53:57 -0700

    [Fix] Fixing test setup

--- tests/resolvers.test.js
+++ tests/resolvers.test.js
@@ -6,7 +6,7 @@ import chai from 'chai';
 var should = chai.expect()
 
 // Imports: Resolvers
-import RESOLVERS from '../server/graphql/resolvers';
+import APP from '../server/server';
 
 
 // BLOG ENTRIES
@@ -14,15 +14,16 @@ describe('Department of Justice (DOJ) Blog Entries', () => {
 
   // Last 10 Blog Entries
   it('Submits an HTTP Request to the API and returns the Last Ten Blog Entries', () => {
-    REQUEST(`http://www.justice.gov/api/v1/blog_entries.json?&pagesize=10`).get(`/`)
-      // Set Headers
-      .set('Accept', 'application/json')
+    REQUEST(APP)
+      // REST API Endpoint
+      .get(`http://www.justice.gov/api/v1/blog_entries.json?&pagesize=10`)
+      // Check Content Type
+      .expect('Content-Type', /json/)
       // Check Status Code
       .expect(200)
       // Check Response Body
       .end(((err, res) => {
-        expect(res.body).to.have.property('metadata')
-        expect(res.body).to.have.property('fuck')
+        console.log('body is: ', res)
       }))
   })
 
@@ -31,9 +32,11 @@ describe('Department of Justice (DOJ) Blog Entries', () => {
     // UUID (ID)
     let UUID = 'e7a19711-08a2-48bf-84af-8a280b2c100a';
 
-    REQUEST(`http://www.justice.gov/api/v1/blog_entries/${UUID}.json?&pagesize=1`).get(`/`)
-      // Set Headers
-      .set('Accept', 'application/json')
+    REQUEST(APP)
+      // REST API Endpoint
+      .get(`http://www.justice.gov/api/v1/blog_entries/${UUID}.json?&pagesize=1`)
+      // Check Content Type
+      .expect('Content-Type', /json/)
       // Check Status Code
       .expect(200)
       // Check Response Body
@@ -51,9 +54,11 @@ describe('Department of Justice (DOJ) Press Releases', () => {
 
   // Last 10 Press Releases
   it('Submits an HTTP Request to the API and returns the Last Ten Press Releases', () => {
-    REQUEST(`http://www.justice.gov/api/v1/press_releases.json?&pagesize=1`).get('/')
-      // Set Headers
-      .set('Accept', 'application/json')
+    REQUEST(APP)
+      // REST API Endpoint
+      .get(`http://www.justice.gov/api/v1/press_releases.json?&pagesize=1`)
+      // Check Content Type
+      .expect('Content-Type', /json/)
       // Check Status Code
       .expect(200)
       // Check Response Body
@@ -67,9 +72,11 @@ describe('Department of Justice (DOJ) Press Releases', () => {
     // UUID (ID)
     let UUID = '8a0f7395-f556-45bf-8890-eeea2d0cbae0';
 
-    REQUEST(`http://www.justice.gov/api/v1/press_releases/${UUID}.json?&pagesize=1`).get('/')
-      // Set Headers
-      .set('Accept', 'application/json')
+    REQUEST(APP)
+      // REST API Endpoint
+      .get(`http://www.justice.gov/api/v1/press_releases/${UUID}.json?&pagesize=1`)
+      // Check Content Type
+      .expect('Content-Type', /json/)
       // Check Status Code
       .expect(200)
       // Check Response Body
@@ -87,9 +94,11 @@ describe('Department of Justice (DOJ) Speeches', () => {
 
   // Last 10 Speeches
   it('Submits an HTTP Request to the API and returns the Last Ten Speeches', () => {
-    REQUEST(`http://www.justice.gov/api/v1/speeches.json?&pagesize=10`).get('/')
-      // Set Headers
-      .set('Accept', 'application/json')
+    REQUEST(APP)
+      // REST API Endpoint
+      .get(`http://www.justice.gov/api/v1/speeches.json?&pagesize=10`)
+      // Check Content Type
+      .expect('Content-Type', /json/)
       // Check Status Code
       .expect(200)
       // Check Response Body
@@ -103,9 +112,11 @@ describe('Department of Justice (DOJ) Speeches', () => {
     // UUID (ID)
     let UUID = '32feb4db-2ab2-425e-b321-7d944cf1767d';    
 
-    REQUEST(`http://www.justice.gov/api/v1/speeches/${UUID}.json?&pagesize=1`).get('/')
-      // Set Headers
-      .set('Accept', 'application/json')
+    REQUEST(APP)
+      // REST API Endpoint
+      .get(`http://www.justice.gov/api/v1/speeches/${UUID}.json?&pagesize=1`)
+      // Check Content Type
+      .expect('Content-Type', /json/)
       // Check Status Code
       .expect(200)
       // Check Response Body
@@ -123,9 +134,11 @@ describe('Department of Justice (DOJ) Vacancy Announcments', () => {
 
   // Last 10 Vacancy Announcments
   it('Submits an HTTP Request to the API and returns the Last Ten Vacancy Announcments', () => {
-    REQUEST(`http://www.justice.gov/api/v1/vacancy_announcements.json?&pagesize=10`).get('/')
-      // Set Headers
-      .set('Accept', 'application/json')
+    REQUEST(APP)
+      // REST API Endpoint
+      .get(`http://www.justice.gov/api/v1/vacancy_announcements.json?&pagesize=10`)
+      // Check Content Type
+      .expect('Content-Type', /json/)
       // Check Status Code
       .expect(200)
       // Check Response Body
@@ -139,9 +152,11 @@ describe('Department of Justice (DOJ) Vacancy Announcments', () => {
     // UUID (ID)
     let UUID = '5b22ae7b-64fd-4fbc-84c6-3fefe4f2ea93';
 
-    REQUEST(`http://www.justice.gov/api/v1/vacancy_announcements/${UUID}.json?&pagesize=1`).get('/')
-      // Set Headers
-      .set('Accept', 'application/json')
+    REQUEST(APP)
+      // REST API Endpoint
+      .get(`http://www.justice.gov/api/v1/vacancy_announcements/${UUID}.json?&pagesize=1`)
+      // Check Content Type
+      .expect('Content-Type', /json/)
       // Check Status Code
       .expect(200)
       // Check Response Body
